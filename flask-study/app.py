100d23064a3f699e112909ed512250adf4af3148
Hyunseung Ryu <56152093+Rhseung@users.noreply.github.com>
2023-05-11 20:54:23 +0900

    flask 공부

---
+++ flask-study/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from markupsafe import escape
+from functools import reduce
+
+app = Flask(__name__)
+
+@app.route('/add/<numbers>')
+def add(numbers):
+    numbers = list(map(int, numbers.split(',')))
+    return f'{sum(numbers)}'
+
+@app.route('/sub/<numbers>')
+def sub(numbers):
+    numbers = list(map(int, numbers.split(',')))
+    return f'{reduce(lambda x, y: x - y, numbers)}'
+
+@app.route('/mul/<numbers>')
+def mul(numbers):
+    numbers = list(map(int, numbers.split(',')))
+    return f'{reduce(lambda x, y: x * y, numbers)}'
+
+@app.route('/div/<numbers>')
+def div(numbers):
+    numbers = list(map(int, numbers.split(',')))
+    return f'{reduce(lambda x, y: x / y, numbers)}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
